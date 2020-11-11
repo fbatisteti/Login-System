99d010200b2f58c343766b1b851acd19f3981707
fbatisteti <fbatisteti@hotmail.com>
2020-11-11 15:55:06 -0300

    Hard reset

---
+++ Server/LoginContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Login.Shared.Models;
+
+namespace Login.Server
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext(DbContextOptions<LoginContext> options) : base(options) { }
+
+        // This here is a list for all the tables that will be in the DB
+        public DbSet<User> Users { get; set; }
+
+        // Managing the DB
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasData(
+                    new User
+                    {
+                        UserId = 1,
+                        Email = "admin@admin.com",
+                        Password = "admin",
+                        Name = "BAMF",
+                        LuckyNumber = 69,
+                        Role = 1
+                    }
+                );
+        }
+    }
+}
---
+++ Shared/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Login.Shared.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid email address", AllowEmptyStrings = false)]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid password (min. 6 characters, max. 255)", AllowEmptyStrings = false)]
+        [Display(Name = "Password")]
+        [StringLength(255, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid screen name (min. 3 characters, max. 20)", AllowEmptyStrings = false)]
+        [Display(Name = "Screen name")]
+        [StringLength(20, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        // No need for tags because it will always have 0 (zero) as default
+        public int LuckyNumber { get; set; }
+
+        // Roles:
+        //    1 - Admin
+        //    2 - Power User
+        //    3 - Normal User
+        public int Role { get; set; }
+    }
+}
